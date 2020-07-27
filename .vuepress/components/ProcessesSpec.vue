e639f6c0f39766b4765a800b3b63c4598daf9674
Matthias Mohr <m.mohr@uni-muenster.de>
2020-07-27 14:23:55 +0200

    Update process docgen

--- .vuepress/components/ProcessesSpec.vue
+++ .vuepress/components/ProcessesSpec.vue
@@ -1,19 +1,30 @@
 <template>
-	<DocGen :document="documentUrl" :apiVersion="version.apiTag" />
+	<component v-if="docgenComponent" :is="docgenComponent" :document="documentUrl" :apiVersion="version.apiTag" />
 </template>
 
 <script>
-import DocGen from '@openeo/processes-docgen/src/DocGen.vue';
+
 import VersioningMixin from '@theme/components/VersioningMixin.vue';
 
 export default {
 	name: 'ProcessesSpec',
 	mixins: [ VersioningMixin ],
-	components: { DocGen },
+	data() {
+		return {
+			docgenComponent: null
+		};
+	},
 	computed: {
 		documentUrl() {
 			return '/documentation/' + this.version.folder + '/processes.json';
 		}
+	},
+	beforeMount() {
+		// See https://vuepress.vuejs.org/guide/using-vue.html#browser-api-access-restrictions
+		import('@openeo/processes-docgen/dist/DocGen.css');
+		import('@openeo/processes-docgen/dist/DocGen.umd.min.js').then(module => {
+      		this.docgenComponent = module.default;
+    	});
 	}
 };
 </script>

9d40198966706343a283c4457c314ba5b2563a1d
Matthias Mohr <webmaster@mamo-net.de>
2024-12-04 16:31:19 +0100

    Update communication channels

--- .vuepress/components/Channels.vue
+++ .vuepress/components/Channels.vue
@@ -2,21 +2,19 @@
   <div class="channels">
     <ul v-if="contact" class="contact">
       <!-- Thankfully provided by https://simpleicons.org/ -->
-      <li><a v-if="email" href="mailto:openeo.psc@uni-muenster.de" target="_blank" class="mail"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/maildotru.svg" alt="Email the PSC" title="Email the PSC" /> Email</a></li>
-      <li><a v-if="chat" href="https://gitter.im/Open-EO/community" target="_blank" class="chat"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/gitter.svg" alt="Public chat on Gitter" title="Public chat on Gitter" /> Chat</a></li>
-      <li><a v-if="github && !code" href="https://github.com/Open-EO" target="_blank" class="github"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/github.svg" alt="GitHub: Open-EO" title="GitHub: Open-EO" /> GitHub</a></li>
-      <li><a v-if="linkedin" href="https://www.linkedin.com/company/openeospec/" target="_blank" class="linkedin"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/linkedin.svg" alt="LinkedIn" title="LinkedIn" /> LinkedIn</a></li>
-      <li><a v-if="x" href="https://twitter.com/Open_EO" target="_blank" class="x"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/x.svg" alt="X / Twitter: Open_EO" title="X / Twitter: Open_EO" /> X / Twitter</a></li>
-      <li><a v-if="youtube" href="https://www.youtube.com/channel/UCMJQil8j9sHBQkcSlSaEsvQ" target="_blank" class="youtube"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/youtube.svg" alt="YouTube: openEO" title="YouTube: openEO" /> YouTube</a></li>
+      <li><a v-if="email" href="mailto:openeo.psc@uni-muenster.de" target="_blank" class="mail"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/maildotru.svg" alt="Email the PSC" title="Email the PSC" /> Email</a></li>
+      <li><a v-if="github" href="https://github.com/Open-EO" target="_blank" class="github"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/github.svg" alt="GitHub: Open-EO" title="GitHub: Open-EO" /> GitHub</a></li>
+      <li><a v-if="linkedin" href="https://www.linkedin.com/company/openeospec/" target="_blank" class="linkedin"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/linkedin.svg" alt="LinkedIn" title="LinkedIn" /> LinkedIn</a></li>
+      <li><a v-if="bluesky" href="https://bsky.app/profile/openeo.org" target="_blank" class="bluesky"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/bluesky.svg" alt="Bluesky: openeo.org" title="Bluesky: openeo.org" /> Bluesky</a></li>
+      <li><a v-if="youtube" href="https://www.youtube.com/channel/UCMJQil8j9sHBQkcSlSaEsvQ" target="_blank" class="youtube"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/youtube.svg" alt="YouTube: openEO" title="YouTube: openEO" /> YouTube</a></li>
     </ul>
     <ul v-if="code" class="code">
-      <li><a v-if="github" href="https://github.com/Open-EO" target="_blank" class="github"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/github.svg" alt="GitHub: Open-EO" title="GitHub: Open-EO" /> GitHub</a></li>
-      <li><a v-if="cran" href="https://cran.r-project.org/package=openeo" target="_blank" class="r"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/r.svg" alt="CRAN" title="CRAN" /> CRAN / R</a></li>
-      <li><a v-if="npm" href="https://www.npmjs.com/search?q=%40openeo%2F" target="_blank" class="npm"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/npm.svg" alt="npm: @openeo" title="npm: @openeo" /> npm / JS</a></li>
-      <li><a v-if="pypi" href="https://pypi.org/project/openeo/" target="_blank" class="pypi"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/pypi.svg" alt="PyPI" title="PyPI" /> PyPI / Python</a></li>
-      <li><a v-if="conda" href="https://anaconda.org/conda-forge/openeo" target="_blank" class="conda"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/anaconda.svg" alt="Conda Forge" title="Conda Forge" /> Conda Forge / Python</a></li>
-      <li><a v-if="julia" href="https://github.com/Open-EO/openeo-julia-client" target="_blank" class="julia"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/julia.svg" alt="Julia" title="Julia" /> Julia</a></li>
-      <li><a v-if="qgis" href="https://plugins.qgis.org/plugins/openeo-qgis-plugin-master/" target="_blank" class="qgis"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10.1.0/icons/qgis.svg" alt="QGIS" title="QGIS" /> QGIS</a></li>
+      <li><a v-if="cran" href="https://cran.r-project.org/package=openeo" target="_blank" class="r"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/r.svg" alt="CRAN" title="CRAN" /> CRAN / R</a></li>
+      <li><a v-if="npm" href="https://www.npmjs.com/search?q=%40openeo%2F" target="_blank" class="npm"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/npm.svg" alt="npm: @openeo" title="npm: @openeo" /> npm / JS</a></li>
+      <li><a v-if="pypi" href="https://pypi.org/project/openeo/" target="_blank" class="pypi"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/pypi.svg" alt="PyPI" title="PyPI" /> PyPI / Python</a></li>
+      <li><a v-if="conda" href="https://anaconda.org/conda-forge/openeo" target="_blank" class="conda"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/anaconda.svg" alt="Conda Forge" title="Conda Forge" /> Conda Forge / Python</a></li>
+      <li><a v-if="julia" href="https://github.com/Open-EO/openeo-julia-client" target="_blank" class="julia"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/julia.svg" alt="Julia" title="Julia" /> Julia</a></li>
+      <li><a v-if="qgis" href="https://plugins.qgis.org/plugins/openeo-qgis-plugin-master/" target="_blank" class="qgis"><img src="https://cdn.jsdelivr.net/npm/simple-icons@13/icons/qgis.svg" alt="QGIS" title="QGIS" /> QGIS</a></li>
     </ul>
   </div>
 </template>
@@ -49,7 +47,7 @@ export default {
       type: Boolean,
       default: true
     },
-    x: {
+    bluesky: {
       type: Boolean,
       default: true
     },
@@ -136,8 +134,8 @@ export default {
 .channels .youtube img
   background-color #00FFFF
 
-.channels .x img
-  background-color #ffffff
+.channels .bluesky img
+  background-color #fd7a00
 
 .channels .r img 
   background-color #D8923C
